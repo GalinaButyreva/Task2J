beb89eb0ed03724bcb472eb3ad939b4dc6c78015
GalinaButyreva <btrgalin@gmail.com>
2024-03-07 09:15:57 +0300

    изменения по оптимизации

--- src/main/java/CacheInvocationHandler.java
+++ src/main/java/CacheInvocationHandler.java
@@ -1,7 +1,6 @@
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 import java.util.*;
 
 public class CacheInvocationHandler implements InvocationHandler {
@@ -17,18 +16,16 @@ public class CacheInvocationHandler implements InvocationHandler {
     private final Map<Method, Object> methodCaches = new HashMap<>();
     // Сравнить на  идентичность методы (имя + пар-ры) (сравнение ссылок  не работает)
     private boolean methodCmp(Method method1, Method method2) {
-        if (method1.getName().equals(method2.getName()) &&
-                Arrays.toString(method1.getParameterTypes()).equals(Arrays.toString(method2.getParameterTypes())))
-                return true;
-        return false;
+        return method1.getName().equals(method2.getName()) &&
+                Arrays.toString(method1.getParameterTypes()).equals(Arrays.toString(method2.getParameterTypes()));
     }
 
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         Object  resultOfInvocation      = null;                 // результат выполнения
-        Boolean isMethodMutator         = false;                // метод явл-ся мутатором
-        Boolean isMethodCache           = false;                // метод с аннотацией Cache
+        boolean isMethodMutator         = false;                // метод явл-ся мутатором
+        boolean isMethodCache           = false;                // метод с аннотацией Cache
 
        // Определяем был ли  вызван метод, аннотируемый CACHE
         for (Method m : methodsCacheList) {
@@ -44,7 +41,7 @@ public class CacheInvocationHandler implements InvocationHandler {
 
         // Если вызван метод  с аннот. CACHE
         if (isMethodCache) {
-            if (methodCaches.containsKey(method) == false || changed == true) {
+            if (methodCaches.containsKey(method) == false || changed) {
                 // Если метод  с аннот. CACHE  был вызван первый раз или значение было изменено методом с аннотацией Mutator
                 resultOfInvocation = method.invoke(targetObject, args);
                 // Фиксируем инфо о вычисленном значении
